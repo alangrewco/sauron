0ac38345e94aa0d2a937cfc6bb85b8cd3f49f1b9
yood2 <yood2@proton.me>
2025-09-13 03:42:55 -0400

    scaffold frontend with shadcn components

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        Sauron
+      </h1>
+    </>
+  );
+}
